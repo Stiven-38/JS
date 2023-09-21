927f9e801858b82467bc8f2346a9bd52f1187ee7
Stiven-38 <129075107+Stiven-38@users.noreply.github.com>
2023-09-21 11:34:25 -0500

    subir

---
+++ actividadswitch.js/js/calcular.js
@@ -0,0 +1,39 @@
+/*
+function calculadora (){
+    opc=parseInt(prompt(" Digite 1 si quiere realizar una suma, digita 2 si quiere realizar una resta, digite 3 si quiere realizar una multiplicacion, digite 4 si quiere realizar una division o digite 5 si quiere saber cual numero es mayor"))
+    
+    switch (opc){ 
+    case 1:
+        m1=parseFloat(prompt("ingrese el primer numero: "))
+        m2=parseFloat(prompt("ingrese el segundo numero "))
+        var resultado=m1+m2
+        document.write(`el resultado de la suma es: ${resultado}`)
+        break;
+    case 2:
+        m1=parseFloat(prompt("ingrese el primer numero: "))
+        m2=parseFloat(prompt("ingrese elsegundo numero "))
+        var resultado=m1-m2
+        document.write(`el resultado de la resta es: ${resultado}`) 
+        break;
+    case 3:
+        m1=parseFloat(prompt("ingrese el primer numero: "))
+        m2=parseFloat(prompt("ingrese el segundo numero: "))
+        var resultado=m1*m2
+        document.write(`el resulatdo de la suma es: ${resultado}`)
+        break;
+    case 4:
+        m1=parseFloat(prompt("ingrese el primer numero: "))
+        m2=parseFloat(prompt("ingrese el segundo numero: "))
+        var resultado=m1/m2
+        document.write(`el valor del residuo es: ${resultado}`)
+        break;
+    case 5:
+        m1=parseFloat(prompt("ingrese el primer numero: "))
+        m2=parseFloat(prompt("ingrese el segundo numero: "))
+        if (m1 > m2)
+            document.write(+m1+ " es mayor que "+m2)
+        else{
+            document.write(+m2+ " es mayor que "+m1)
+        }
+    }}
+*/
---
+++ actividadswitch.js/js/swicht.js
@@ -0,0 +1,37 @@
+/*
+function mostrar (){
+    function cuadrado(){
+        m1=parseInt(prompt("ingrse el valor de de un lado del cuadrado: "))
+        var resultado=m1**2
+        document.write(`el resultado del valor del residuo es: ${resultado}`)
+    }
+    function rectangulo(){
+        m1=parseInt(prompt("ingrese la base del rectangulo: "))
+        m2=parseInt(prompt("ingrese la altura del rectangulo "))
+        var resultado=m1*m2
+        document.write(`el valor  del residuo es: ${resultado}`) 
+    }
+    function triangulo(){
+        m1=parseInt(prompt("ingrese el primer numero: "))
+        m2=parseInt(prompt("ingrese el segundo numero: "))
+        var resultado=m1*m2/2;
+        document.write(`el valor del residuo es: ${resultado}`)
+    }
+    
+    opc=parseInt(prompt("ingrese opcion 1: para el cuadrado, opcion 2: para rectangulo ,opcion 3:trinagulo"))
+    switch(opc){
+        case 1:
+            cuadrado()
+            break;
+            case 2:
+                rectangulo()
+                break;
+                case 3:
+                    triangulo()
+                    break;
+                    
+                default:
+                    alert("la opcion no es correcta")
+    }
+} 
+*/
