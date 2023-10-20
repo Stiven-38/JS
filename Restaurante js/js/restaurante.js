d7e02b7d503a26b10638ffad0c9ed635f3dcdec9
Stiven-38 <129075107+Stiven-38@users.noreply.github.com>
2023-10-20 15:46:34 -0500

    .
    .

---
+++ Restaurante js/js/restaurante.js
@@ -0,0 +1,25 @@
+// function pedido() {
+//     var tipoPlato = document.getElementById("tipoPlato").value;
+//     var cantidadPlatos = parseInt(document.getElementById("cantidadPlatos").value);
+//     var totalBebidas = document.getElementById("bebida").value;
+//     var cantidadBebidas = parseInt(document.getElementById("cantidadBebidas").value);
+
+//     var precioPlatos = {
+//         "Ajiaco": 40000,
+//         "Arroz con paisa": 35000,
+//         "Caldo Costilla": 15000
+//     };
+
+//     var precioBebida = {
+//         "Gaseosa":3000,
+//         "Limonada": 2500,
+//         "Jugo Naranja":2000
+//     };
+
+//     var totalPlatos = precioPlatos[tipoPlato] * cantidadPlatos;
+//     var totalBebidas = precioBebida * cantidadBebidas;
+//     var total = totalPlatos + totalBebidas;
+
+//     var resultado = document.getElementById("resultado");
+//     resultado.textContent = "Total a pagar: $" + total;
+// }
